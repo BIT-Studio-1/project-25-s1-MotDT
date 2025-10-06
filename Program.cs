832a36ca56b4892440735b6e3af65bfee4d3771c
Benjamin Nikou (1000114581) <NIKOB2@student.op.ac.nz>
2025-10-07 11:40:10 +1300

    Spelling fixes and health bar clarification

--- Program.cs
+++ Program.cs
@@ -6,46 +6,47 @@ namespace Studio_1
     {
         static void Main(string[] args)
         {
+            // Main logic here
+        }
 
-        
         static void PrintHealthBar(Character character)
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            string remainingHealthBar = new string('█', character.health.maxHP);
-            string totalHealthBar = new string('_',character.health.maxHP - character.health.curHP);
+            string remainingHealthBar = new string('█', character.health.curHP);
+            string totalHealthBar = new string('_', character.health.maxHP - character.health.curHP);
             Console.WriteLine($"{character.name} HP: {remainingHealthBar}{totalHealthBar} ({character.health.curHP}/{character.health.maxHP})");
             Console.ForegroundColor = ConsoleColor.White;
         }
     }
-        public class Character //idea with this character is way more simple  you test him self against dc10+monster attack
-        {
-            /// Character's health component
-            public EntityHealth health;
-            public string name;
-            int damDice;
-            int strength; // this is used at hit mod i am stupid -Rory
-            int fineese;
-            int toughness;
-            int presnase;
-        }
 
-        public class Monster
-        {
-            /// Monster's health component
-            public EntityHealth health;
-            public string name;
-            int damDice;
-            int dodgeDiff;
-            int hitDiff;
-            bool item1;
-        }
+    public class Character // This character is simpler; you test themself against DC10+ monster attack
+    {
+        /// Character's health component
+        public EntityHealth health;
+        public string name;
+        int damDice;
+        int strength; // this is used as hit mod
+        int finesse;
+        int toughness;
+        int presence;
+    }
 
-        /// Health class used by both players and monsters
-        public class EntityHealth {
-            /// Max health of the entity
-            public int maxHP;
-            /// Current Health of the entity
-            public int curHP;
-        }
+    public class Monster
+    {
+        /// Monster's health component
+        public EntityHealth health;
+        public string name;
+        int damDice;
+        int dodgeDiff;
+        int hitDiff;
+        bool item1;
+    }
+
+    /// Health class used by both players and monsters
+    public class EntityHealth {
+        /// Max health of the entity
+        public int maxHP;
+        /// Current Health of the entity
+        public int curHP;
     }
 }
