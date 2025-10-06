9f453d71e3e2982bf9db5a969308e595ef8041dc
Rory Henry-Osborne (1000067845) <HENRR4@student.op.ac.nz>
2025-10-07 10:32:30 +1300

    Inital Character stuff added to main program

--- Program.cs
+++ Program.cs
@@ -2,10 +2,30 @@
 {
     internal class Program
     {
-        // Causes entrypoint errors otherwise
-        // static void Main(string[] args)
-        // {
-        //     Console.WriteLine("Hello, World!");
-        // }
+        static void Main(string[] args)
+        {
+                public class Character //idea with this character is way more simple  you test him self against dc10+monster attack
+                {
+                    string name;
+                    int maxHP;
+                    int curHP;
+                    int hitMod;
+                    int damDice;
+                    int strength;
+                    int fineese;
+                    int toughness;
+                    int presnase;
+                }
+
+                public class monster
+                {
+                    string name;
+                    int maxHP;
+                    string curHP;
+                    int damDice;
+                    int attackBonus;
+                    bool item1;
+                }
+    }
     }
 }
