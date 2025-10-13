2e24677df42fb698cd02c39ff747a8d8843300fc
Rory Henry-Osborne (1000067845) <HENRR4@student.op.ac.nz>
2025-10-14 10:53:54 +1300

    bad combat logic temp fix

--- Program.cs
+++ Program.cs
@@ -369,31 +369,37 @@ namespace Studio_1
 
         public static void Combat(ref Character hero, ref Monster monster)
         {
-            Random random = new Random();
-            int player_roll = Roll(hero.strength);
-            if (player_roll >= monster.hitDiff)
+            do
             {
-                int dam = random.Next(1, hero.damDice + 1);
-                monster.health.curHP -= dam;
-                Console.WriteLine($"You strike the {monster.name} for {dam}");
-            }
-            else
-            {
-                Console.WriteLine($"{hero.name} Strikes the {monster.name} and misses");
-            }
+                Random random = new Random();
+                int player_roll = Roll(hero.strength);
+                if (player_roll >= monster.hitDiff)
+                {
+                    int dam = random.Next(1, hero.damDice + 1);
+                    monster.health.curHP -= dam;
+                    Console.WriteLine($"You strike the {monster.name} for {dam}");
+                }
+                else
+                {
+                    Console.WriteLine($"{hero.name} Strikes the {monster.name} and misses");
+                }
 
-            //Monster 'attacks'
-            player_roll = Roll(hero.finesse);
-            if (player_roll <= monster.dodgeDiff)
-            {
-                int dam = random.Next(1, monster.damDice + 1);
-                hero.health.curHP -= dam;
-                Console.WriteLine($"{monster.name} strikes you for {dam}");
-            }
-            else
-            {
-                Console.WriteLine($"{hero.name} dodges the {monster.name}'s attack just in time");
-            }
+                if (monster.health.curHP > 0)
+                {
+                    //Monster 'attacks'
+                    player_roll = Roll(hero.finesse);
+                    if (player_roll <= monster.dodgeDiff)
+                    {
+                        int dam = random.Next(1, monster.damDice + 1);
+                        hero.health.curHP -= dam;
+                        Console.WriteLine($"{monster.name} strikes you for {dam}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{hero.name} dodges the {monster.name}'s attack just in time");
+                    }
+                }
+            } while (monster.health.curHP > 0 || monster.health.curHP > 0);
         }
 
     }
