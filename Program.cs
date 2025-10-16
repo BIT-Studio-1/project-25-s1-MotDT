c977d37eaa0353514daa0ffa25f1d8ccef3c9b0d
Andy Wu (1000135858) <WUA2@student.op.ac.nz>
2025-10-16 14:25:55 +1300

    Added more constants for text colour and formatting

--- Program.cs
+++ Program.cs
@@ -14,11 +14,23 @@ namespace Studio_1
             public Random random_gen;
         }
 
-        //Global constants for changing colour of text
+        //Global constants for changing colour of text, remember to always RESET!
         public const string RESET = "\x1b[0m";
         public const string RED = "\x1b[91m";
         public const string GREEN = "\x1b[92m";
+        public const string YELLOW = "\x1b[93m";
         public const string BLUE = "\x1b[94m";
+        public const string MAGENTA = "\x1b[95m";
+        public const string CYAN = "\x1b[96m";
+        public const string GREY = "\x1b[97m";
+
+        //Global constants for changing formatting of text, remember to cancel the formatting after!
+        public const string BOLD = "\x1b[1m";
+        public const string NOBOLD = "\x1b[22m";
+        public const string UNDERLINE = "\x1b[4m";
+        public const string NOUNDERLINE = "\x1b[24m";
+        public const string REVERSE = "\x1b[7m";
+        public const string NOREVERSE = "\x1b[27m";
 
         static void Main()
         {
