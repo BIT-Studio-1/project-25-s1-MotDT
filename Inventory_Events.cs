2e9b68291fd2bf680cfccf0d52faa039c70ba4e3
Andy Wu (1000135858) <WUA2@student.op.ac.nz>
2026-03-05 15:50:13 +1300

    Create Inventory_Events.cs

---
+++ Inventory_Events.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studio_1
+{
+    internal class Inventory_Events
+    {
+        public class InventoryAndEvents
+        {
+            public Dictionary<string, int> inventory = new Dictionary<string, int>();
+            public Dictionary<string, bool> events = new Dictionary<string, bool>();
+        }
+    }
+}
